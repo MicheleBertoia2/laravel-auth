01bbf11716a2b4dea6f66df986b27f9b1eca0667
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-16 15:28:54 +0200

    [add] aside bar and [moved] header in a partial folder

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,7 @@
+<aside class="p-5">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Elenco Progetti</a></li>
+        <li><a href="#">Aggiungi un nuovo progetto</a></li>
+    </ul>
+</aside>
