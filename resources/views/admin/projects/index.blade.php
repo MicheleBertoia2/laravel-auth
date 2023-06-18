1f60e32456bb66f41cc3074bb94728a45809c533
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-18 11:26:21 +0200

    [add] projects seeder and project index view

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,5 +4,29 @@
     <div class="container">
         <h1 class="my-5">Elenco progetti</h1>
 
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Repo Link</th>
+                <th scope="col">Collaboratori</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project )
+
+                <tr>
+                  <td>{{$project->id}}</td>
+                  <td>{{$project->title}}</td>
+                  <td>{{$project->project_link}}</td>
+                  <td>{{$project->collaborators}}</td>
+                </tr>
+
+                @endforeach
+
+            </tbody>
+          </table>
+
     </div>
 @endsection
