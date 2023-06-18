672e2361b3ec081f03c01626f31a629a21001158
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-18 12:25:50 +0200

    [finish] create form view

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -46,6 +46,97 @@
                   @enderror
             </div>
 
+            <div class="mb-3">
+                <label for="image_paths" class="form-label">Path immagini (separati da |)</label>
+                <input
+                  type="text"
+                  class="form-control @error('image_paths') is-invalid @enderror"
+                  id="image_paths"
+                  name="image_paths"
+                  value="{{ old('image_paths') }}">
+                  @error('image_paths')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="project_link" class="form-label">Link della Repo</label>
+                <input
+                  type="text"
+                  class="form-control @error('project_link') is-invalid @enderror"
+                  id="project_link"
+                  name="project_link"
+                  value="{{ old('project_link') }}">
+                  @error('project_link')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="collaborators" class="form-label">Collaboratori (separati da |)</label>
+                <input
+                  type="text"
+                  class="form-control @error('collaborators') is-invalid @enderror"
+                  id="collaborators"
+                  name="collaborators"
+                  value="{{ old('collaborators') }}">
+                  @error('collaborators')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="frameworks" class="form-label">Frameworks usati (separati da |)</label>
+                <input
+                  type="text"
+                  class="form-control @error('frameworks') is-invalid @enderror"
+                  id="frameworks"
+                  name="frameworks"
+                  value="{{ old('frameworks') }}">
+                  @error('frameworks')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="prog_languages" class="form-label">Linguaggi di programmazione usati (separati da |)</label>
+                <input
+                  type="text"
+                  class="form-control @error('prog_languages') is-invalid @enderror"
+                  id="prog_languages"
+                  name="prog_languages"
+                  value="{{ old('prog_languages') }}">
+                  @error('prog_languages')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="date_started" class="form-label">Data inizio progetto</label>
+                <input
+                  type="date"
+                  class="form-control @error('date_started') is-invalid @enderror"
+                  id="date_started"
+                  name="date_started"
+                  value="{{ old('date_started') }}">
+                  @error('date_started')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="date_ended" class="form-label">Data fine progetto</label>
+                <input
+                  type="date"
+                  class="form-control @error('date_ended') is-invalid @enderror"
+                  id="date_ended"
+                  name="date_ended"
+                  value="{{ old('date_ended') }}">
+                  @error('date_ended')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+            </div>
+
             <button type="submit" class="btn btn-primary">Crea!</button>
 
 
